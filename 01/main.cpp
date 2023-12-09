c7753c7d35b7e11ad39490390d72b7296adb7369
GluXoy <mahaonterricon@gmail.com>
2023-12-09 21:26:10 +0300

    add lifes, crane sprites

--- 01/main.cpp
+++ 01/main.cpp
@@ -5,92 +5,135 @@
 #include <vector>
 #include <chrono>
 #include <random>
+#include <string>
 
 using namespace std;
 
 struct Block
 {
     sf::RectangleShape shape;
-    sf::Vector2f position; //заменить позже на константу начальных координат появляющегося блока на кране
+    sf::Vector2f position;
     sf::Vector2f size;
-    sf::Color color = sf::Color(120, 35, 10);
-
-    float craneBlockSpeed = 700.0f;
+    sf::Color darkenColor = sf::Color(200, 100, 100);
+    float craneBlockSpeed = 550.0f;
     float fallenBlockSpeed = 700.0f;
+};
+
+struct BrokenBlockEvents {
+    float brokenWidth = 0;
+    Block brokenBlock;
+    bool leftZone = false;
+    bool rightZone = false;
 };
 
 struct GameEvents
 {
     sf::VideoMode desktopMode;
+    sf::RectangleShape darknessRect;
     bool isNewBlock = true;
     bool isBlockFalling = false;
     float dTime;
     float leftDropZone;
     float rightDropZone;
-    sf::Sprite background;
+    sf::Texture startBlockTexture;
+    sf::Texture brokenBlockTexture;
+    sf::Texture blockTexture;
+    sf::Texture heartTexture;
+    BrokenBlockEvents brokenBlockEvents;
+    int lifes = 3;
+    sf::RectangleShape box;
+    vector<sf::Sprite> hearts;
 };
 
-Block initBlock(Block& prevBlock, const sf::Texture& blockTexture, GameEvents& gameEvents) {
+Block initBlock(Block& prevBlock, GameEvents& gameEvents) {
     static std::mt19937 gen(std::chrono::system_clock::now().time_since_epoch().count());
-    std::uniform_int_distribution<int> disX(0, gameEvents.desktopMode.width - static_cast<int>(prevBlock.size.x));
+    std::uniform_int_distribution<int> disX(500, gameEvents.desktopMode.width - 400 - static_cast<int>(prevBlock.size.x));
 
     Block block;
     block.size = prevBlock.size;
     block.shape.setSize(prevBlock.size); 
-    block.shape.setTexture(&blockTexture);
+    block.shape.setTexture(&gameEvents.blockTexture);
 
-    // Генерация случайной позиции по оси X
+    block.shape.setFillColor(block.darkenColor);
+
     const float randomX = static_cast<float>(disX(gen));
-    
-    // Позиция блока с учетом случайной координаты X
-    block.position = {randomX, 0}; 
+    block.position = {randomX, 230}; 
 
     block.shape.setPosition(block.position);
     return block;
 }
 
 void updateBlockOnCrane(Block& block, GameEvents& gameEvents) {
-    if (!gameEvents.isBlockFalling) { 
-        float dTime = gameEvents.dTime;
-        const float speed = block.craneBlockSpeed;
-        const float offset = speed * dTime;
+    if (gameEvents.isBlockFalling) { 
+        return;
+    }
+    float dTime = gameEvents.dTime;
+    const float speed = block.craneBlockSpeed;
+    const float offset = speed * dTime;
 
-        const float screenWidth = gameEvents.desktopMode.width;
-        const float blockPositionX = block.position.x;
-        const float blockWidth = block.size.x;
+    const float screenWidth = gameEvents.desktopMode.width - 290;
+    const float blockPositionX = block.position.x;
+    const float blockWidth = block.size.x;
 
-        if ( ((blockPositionX + blockWidth + offset >= screenWidth) && (speed > 0)) 
-        || ((blockPositionX + offset <= 0) && (speed < 0)) ) 
-        {
-            block.craneBlockSpeed = -block.craneBlockSpeed;
-        }
-        else
-        {
-            block.position.x = blockPositionX + offset;
-            block.shape.setPosition(block.position);
-        }
+    if ( ((blockPositionX + blockWidth + offset >= screenWidth) && (speed > 0)) 
+    || ((blockPositionX + offset <= 400) && (speed < 0)) ) 
+    {
+        block.craneBlockSpeed = -block.craneBlockSpeed;
+    }
+    else
+    {
+        block.position.x = blockPositionX + offset;
+        block.shape.setPosition(block.position);
     }
 }
 
+void initBrokenBlock(GameEvents& gameEvents, const float buildingHeight, float blockHeight) {
+    Block block;
+    block.position.y = buildingHeight;    
+    float posX;
+    if (gameEvents.brokenBlockEvents.leftZone) 
+        posX = gameEvents.leftDropZone - gameEvents.brokenBlockEvents.brokenWidth;
+    else  
+        posX = gameEvents.rightDropZone;    
+    block.position.x = posX;
+    gameEvents.brokenBlockEvents.leftZone = false;
+    gameEvents.brokenBlockEvents.rightZone = false;
+
+    block.size.x = gameEvents.brokenBlockEvents.brokenWidth;
+    block.size.y = blockHeight;
+    block.shape.setSize(block.size);
+
+    block.shape.setTexture(&gameEvents.brokenBlockTexture);
+    block.shape.setFillColor(block.darkenColor);
+
+    block.shape.setPosition(block.position);
+    gameEvents.brokenBlockEvents.brokenBlock = block;
+}
+
 void collisionBlock(Block& block, GameEvents& gameEvents, const float buildingHeight) {
+    float delta;
     if (block.position.x < gameEvents.leftDropZone) {
-        float delta = gameEvents.leftDropZone - block.position.x;
+        delta = gameEvents.leftDropZone - block.position.x;
         sf::Vector2f newPosition = {gameEvents.leftDropZone, buildingHeight - block.size.y};
         block.size.x = block.size.x - delta;
         block.shape.setSize(block.size);
         block.position = newPosition;
         block.shape.setPosition(newPosition);
         gameEvents.rightDropZone = gameEvents.leftDropZone + block.size.x;
+        gameEvents.brokenBlockEvents.leftZone = true;
     }
     else {
-        float delta = block.position.x + block.size.x - gameEvents.rightDropZone;
+        delta = block.position.x + block.size.x - gameEvents.rightDropZone;
         block.size.x = block.size.x - delta;
         block.shape.setSize(block.size);
         block.position.y = buildingHeight - block.size.y;
         block.shape.setPosition(block.position);
         gameEvents.rightDropZone = block.position.x + block.size.x;
         gameEvents.leftDropZone = gameEvents.rightDropZone - block.size.x;
+        gameEvents.brokenBlockEvents.rightZone = true;
     }
+    gameEvents.brokenBlockEvents.brokenWidth = delta;
+    initBrokenBlock(gameEvents, buildingHeight, block.size.y);
 }
 
 
@@ -113,6 +156,7 @@ void updateFallingBlock(Block& block, vector<Block>& building, GameEvents& gameE
         if (!((block.position.x + blockWidth <= gameEvents.leftDropZone) || (block.position.x >= gameEvents.rightDropZone))) {
             gameEvents.isBlockFalling = false;
             collisionBlock(block, gameEvents, buildingHeight);
+            block.shape.setFillColor(sf::Color::White);
             building.push_back(block);
             gameEvents.isNewBlock = true;
             return;
@@ -122,6 +166,7 @@ void updateFallingBlock(Block& block, vector<Block>& building, GameEvents& gameE
         block.size = {0,0};
         gameEvents.isBlockFalling = false;
         gameEvents.isNewBlock = true;
+        gameEvents.lifes--;
         return;
     }
     block.shape.setPosition(block.position);
@@ -150,20 +195,26 @@ void pollEvents(sf::RenderWindow& window, GameEvents& gameEvents) {
     }
 }
 
-void redrawFrame(sf::RenderWindow& window, Block& block, vector<Block>& building, sf::Sprite& backgroundSprite) {
+void redrawFrame(sf::RenderWindow& window, Block& block, vector<Block>& building, sf::Sprite& backgroundSprite, GameEvents& gameEvents) {
     window.clear();
     
     window.draw(backgroundSprite);
-    sf::RectangleShape darknessRect(sf::Vector2f(window.getSize().x, window.getSize().y));
-    darknessRect.setFillColor(sf::Color(0, 0, 0, static_cast<sf::Uint8>(0.5 * 255)));
-    window.draw(darknessRect);
+    window.draw(gameEvents.darknessRect);
 
     for (size_t i = 0; i < building.size(); i++) {
         window.draw(building[i].shape);
     }
 
+    window.draw(gameEvents.box);
+    for (size_t i = 0; i < gameEvents.lifes; i++) {
+        window.draw(gameEvents.hearts[i]);
+    }
+
     window.draw(block.shape);
-    window.display();
+    if (gameEvents.brokenBlockEvents.brokenWidth != 0)
+        window.draw(gameEvents.brokenBlockEvents.brokenBlock.shape);
+    
+    // window.display();
 }
 
 void updateBuildingPositions(GameEvents& gameEvents, vector<Block>& building, float newBuildingHeight) {
@@ -177,7 +228,7 @@ void updateBuildingPositions(GameEvents& gameEvents, vector<Block>& building, fl
 }
 
 void updateBuildingHeight(vector<Block>& building, GameEvents& gameEvents) {
-    if (building.size() > 7) {
+    if (building.size() > 8) {
         float newBuildingHeight = building[0].size.y * building.size() - building[0].size.y;
         building.erase(building.begin());
         
@@ -185,15 +236,31 @@ void updateBuildingHeight(vector<Block>& building, GameEvents& gameEvents) {
     }
 }
 
+void updateBrokenBlock(GameEvents& gameEvents) {
+    if (gameEvents.brokenBlockEvents.brokenWidth == 0) return;
+    float offset = gameEvents.dTime * gameEvents.brokenBlockEvents.brokenBlock.fallenBlockSpeed;
+    gameEvents.brokenBlockEvents.brokenBlock.position.y += offset;
+    gameEvents.brokenBlockEvents.brokenBlock.shape.setPosition(gameEvents.brokenBlockEvents.brokenBlock.position);
+    if (gameEvents.brokenBlockEvents.brokenBlock.position.y >= gameEvents.desktopMode.height) {
+        gameEvents.brokenBlockEvents.brokenBlock.size = {0,0};
+        gameEvents.brokenBlockEvents.brokenWidth = 0;
+        return;
+    }
+}
+
 void gameProcess(GameEvents& gameEvents, vector<Block>& building, Block& block) {
     updateBlockOnCrane(block, gameEvents);
     updateFallingBlock(block, building, gameEvents); 
+    updateBrokenBlock(gameEvents);
     updateBuildingHeight(building, gameEvents);  
 }
 
 void initStartBlock(vector<Block>& building, GameEvents& gameEvents) {
+    if (!gameEvents.startBlockTexture.loadFromFile("bricks/wall8.png")) {
+        cerr << "Failed to load startBlockTexture" << endl;
+    }
     Block startBlock;
-    const sf::Vector2f START_BLOCK_SIZE = { 250, 100 };
+    const sf::Vector2f START_BLOCK_SIZE = { 190, 60 };
     startBlock.size = START_BLOCK_SIZE;
     startBlock.shape.setSize(startBlock.size); 
 
@@ -203,6 +270,7 @@ void initStartBlock(vector<Block>& building, GameEvents& gameEvents) {
     };
 
     startBlock.position = basePosition;
+    startBlock.shape.setTexture(&gameEvents.startBlockTexture);
     startBlock.shape.setPosition(basePosition); 
     
     building.push_back(startBlock);
@@ -211,6 +279,43 @@ void initStartBlock(vector<Block>& building, GameEvents& gameEvents) {
     gameEvents.rightDropZone = building[0].position.x + building[0].size.x;
 }
 
+void initHeart(GameEvents& gameEvents) {
+    sf::Sprite heart;
+    const sf::Vector2f size = {80.0f, 80.0f};
+    heart.setTexture(gameEvents.heartTexture);
+    heart.setScale(size.x / gameEvents.heartTexture.getSize().x, size.y / gameEvents.heartTexture.getSize().y);
+    gameEvents.hearts.push_back(heart);
+}
+
+void initLifesBox(GameEvents& gameEvents) {
+    if (!gameEvents.heartTexture.loadFromFile("other/life.png")) {
+        cerr << "Failed to load heart texture" << endl;
+    }
+    gameEvents.box.setSize({400, 100});
+    gameEvents.box.setPosition({1520, 0});
+    gameEvents.box.setFillColor(sf::Color::Transparent);
+
+    float distanceBetweenHearts = 30.0f;
+    sf::Vector2f startPosition(gameEvents.desktopMode.width - 110.0f, gameEvents.box.getPosition().y + 30.0f);
+
+    for (int i = 0; i < gameEvents.lifes; i++) {
+        initHeart(gameEvents);
+        gameEvents.hearts[i].setPosition(startPosition);
+        startPosition.x -= gameEvents.hearts[i].getGlobalBounds().width + distanceBetweenHearts;
+    }
+}
+
+void initBrokenBlockTexture(GameEvents& gameEvents) {
+    if (!gameEvents.brokenBlockTexture.loadFromFile("bricks/wall8.png")) {
+        cerr << "Failed to load brokenBlockTexture" << endl;
+    }
+}
+
+void initBlockTexture(GameEvents& gameEvents) {
+    if (!gameEvents.blockTexture.loadFromFile("bricks/wall8.png")) {
+        cerr << "Failed to load blockTexture" << endl;
+    }
+}
 
 int main() {
 
@@ -236,30 +341,61 @@ int main() {
     sf::Clock clock;
     GameEvents gameEvents;
     gameEvents.desktopMode = desktopMode;
+
+    const float DARKNESS_DEGREE = 0.75;
+    sf::RectangleShape darknessRect(sf::Vector2f(window.getSize().x, window.getSize().y));
+    darknessRect.setFillColor(sf::Color(0, 0, 0, static_cast<sf::Uint8>(DARKNESS_DEGREE * 255)));
+    gameEvents.darknessRect = darknessRect;
+
     vector<Block> building;
 
-    sf::Texture startBlockTexture;
-    if (!startBlockTexture.loadFromFile("bricks/wall2.png")) {
-        cerr << "Failed to load texture" << endl;
-        return -1;
-    }
     initStartBlock(building, gameEvents);
-    building[0].shape.setTexture(&startBlockTexture);
-
+    initLifesBox(gameEvents);
+    initBrokenBlockTexture(gameEvents);
+    initBlockTexture(gameEvents);
+    
     Block newBlock;
 
-    while (window.isOpen()) {
+    sf::Texture ctexture;
+    if (!ctexture.loadFromFile("other/crane.png")) {
+        cerr << "Failed to load crane texture" << endl;
+        return -1;
+    }
+    sf::Sprite crane;
+    const sf::Vector2f size = {1920.0f, 1080.0f};
+    crane.setTexture(ctexture);
+    crane.setScale(size.x / ctexture.getSize().x, size.y / ctexture.getSize().y);
+
+    crane.setPosition({ 100, 0});
+
+    sf::Texture hookTexture;
+    if (!hookTexture.loadFromFile("other/hook.png")) {
+        cerr << "Failed to load hook texture" << endl;
+        return -1;
+    }
+    sf::Sprite hook;
+    const sf::Vector2f sizeHook = { 130.0f, 150.0f };
+    hook.setTexture(hookTexture);
+    hook.setScale(sizeHook.x / hookTexture.getSize().x, sizeHook.y / hookTexture.getSize().y);
+
+    hook.setPosition({ 900, 135});
+
+
+    while (window.isOpen() && gameEvents.lifes) {
         float dTime = clock.restart().asSeconds();
         gameEvents.dTime = dTime;
 
         if (gameEvents.isNewBlock) {
-            newBlock = initBlock(building[building.size() - 1], startBlockTexture, gameEvents);
+            newBlock = initBlock(building[building.size() - 1], gameEvents);
             gameEvents.isNewBlock = false;
         }
 
         pollEvents(window, gameEvents);
         gameProcess(gameEvents, building, newBlock);
-        redrawFrame(window, newBlock, building, backgroundSprite);
+        redrawFrame(window, newBlock, building, backgroundSprite, gameEvents);
+        window.draw(crane);
+        window.draw(hook);
+        window.display();
     }
 
 }
